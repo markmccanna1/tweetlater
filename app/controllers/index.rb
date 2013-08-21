05164039a8d6dd8059f7c32c8904a244cc44e657
Mark McCanna <markmccanna1@gmail.com>
2013-08-20 19:09:24 -0500

    pushing to heroku

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -17,9 +17,12 @@ get '/auth' do
   @access_token = request_token.get_access_token(:oauth_verifier => params[:oauth_verifier])
   # our request token is only valid until we use it to get an access token, so let's delete it from our session
   session.delete(:request_token)
-
+  puts @access_token.inspect
+  puts params.inspect
   # at this point in the code is where you'll need to create your user account and store the access token
-
+  @user = User.where(oauth_token: params[:oauth_token], 
+                     oauth_secret: params[:oauth_verifier], 
+                     username: @access_token.params[:screen_name]).first_or_create
   erb :index
   
 end
